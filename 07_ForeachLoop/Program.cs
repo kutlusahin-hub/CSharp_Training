e71f1e41c209e110bb7d8c53f648cf2de0f139c6
Ktulu <muhammedkutlu55@gmail.com>
2025-12-08 12:34:56 +0300

    07_ForeachLoop

---
+++ 07_ForeachLoop/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace _07_ForeachLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Foreach Döngüsü
+
+            // Foreach (1;2;3;4)
+
+            // 1: Değişken Türü
+            // 2: Değişken Adı
+            // 3: In
+            // 4: Liste, Koleksiyon, Dizi
+
+            //string[] cities = { "milano", "roma", "budapeşte", "ankara", "istanbul", "varşova" };
+
+            //foreach (string x in cities)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //int[] numbers = { 17, 58, 275, 8, 587, 45, 26, 89, 19674, 9375, 134};
+
+            //foreach(int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //int[] numbers = { 17, 58, 275, 8, 587, 45, 26, 89, 19674, 9375, 134 };
+
+            //foreach (int number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //    {
+            //        Console.WriteLine(number);
+            //    }
+            //}
+
+            //int[] numbers = { 17, 58, 275, 8, 587, 45, 26, 89, 19674, 9375, 134 };
+
+            //int total = 0;
+
+            //foreach(int i in numbers)
+            //{
+            //    total += i;
+            //}
+            //Console.WriteLine(total);
+
+            // Foreach döngüsü sadece dizilerle çalışan bir yapı değil,
+            // aynı zamanda listelerle de çalışır.
+            // hem dizi hem liste birer koleksiyon öğesidir.
+
+            //List<int> numbers = new List<int>()
+            //{
+            //    1,2,3,4,5,8
+            //};
+
+            //foreach (int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            //string word = "Merhaba";
+            //foreach (char c in word)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+
+            #endregion
+
+            #region Örnek Sınav Sistemi Uygulaması
+
+            Console.Write("***** C# Eğitim Kampı Sınav Uygulaması*****");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // Sınıftaki öğrenci sayısını kullanıcıdan alma
+            Console.WriteLine("------------------------------");
+            Console.Write("Sınıfınızda Kaç Öğrenci Var: ");
+            int studentCount = int.Parse(Console.ReadLine());
+            Console.WriteLine("------------------------------");
+
+            // Öğrenci isimlerini ve not ortalamalarını saklayacak diziler
+            string[] studentNames = new string[studentCount];
+            double[] studentExamAvg = new double[studentCount];
+
+            for(int i=0; i<studentCount; i++)
+            {
+                Console.Write($"{i+1}. öğrencinin ismini giriniz: "); // $"{değişken} . . . "
+                studentNames[i] = Console.ReadLine();                 // Tırnak içerisindeki değeri okutur.
+                
+                double totalExamResult = 0;
+
+                // Her öğrenci için 3 sınav notu girişi 
+
+                for(int j=0; j<3; j++)
+                {
+                    Console.Write($"{studentNames[i]} adlı öğrencinin {j + 1}. sınav notunu giriniz: ");
+                    double value = double.Parse(Console.ReadLine());
+                    totalExamResult += value; // notları topluyoruz.
+                }
+                
+                Console.WriteLine();
+                studentExamAvg[i] = totalExamResult / 3;
+            }
+
+            // Sınav Ortalamaları
+            for(int i=0; i<studentCount; i++)
+            {
+                Console.WriteLine("-----------------------------");
+
+                Console.WriteLine($"{studentNames[i]} adlı öğrencinin ortalaması: {studentExamAvg[i]}");
+
+                // Öğrencilerin ortalaması ve geçip kalma durumları
+                if (studentExamAvg[i] >= 50)
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersi geçti.");
+                }
+                else
+                {
+                    Console.WriteLine($"{studentNames[i]} adlı öğrenci dersten kaldı.");
+                }
+                
+                Console.WriteLine("-----------------------------");
+            }
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
